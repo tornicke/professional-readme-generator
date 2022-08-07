8c552fd071f0e6756ee46196fe5e363d318371fb
tornicke <torniqe@gmail.com>
2022-08-07 19:13:54 +0200

    Removing the unnecessary message for Badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -72,9 +72,6 @@ const generateTemplate = (answer) => {
   ## Badges
   
   ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
 `;
   return readmeTemplate;
 };
