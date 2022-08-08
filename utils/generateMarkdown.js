4ff3bb5921d7687a2de409a32d3d64550cecbd2a
tornicke <torniqe@gmail.com>
2022-08-08 19:16:06 +0200

    Taking out unnecessary comments from generateMarkd

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 // render license badge based on the user's choice of license
 function renderLicenseBadge(license) {
   return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`;
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 const generateTemplate = (answer) => {
